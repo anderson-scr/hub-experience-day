40ab81e82be81c6c9551edb54655ebd3268d3122
Anderson <anderson.scr@outlook.com>
2022-06-01 20:48:52 -0400

    Comecando o site do senac.

---
+++ Components/Fetchs/fetch.js
@@ -0,0 +1,30 @@
+const fetchHeader = new Promise((resolve) => {
+  
+  // Fetch do Header
+  fetch("../Components/NavBar/navBar.html")
+  .then( response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("#header").innerHTML = data
+  })
+
+  // Fetch do campo azul
+  fetch("../Components/CampoAzul/campoAzul.html")
+  .then( response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("#campoAzul").innerHTML = data
+  })
+
+  // Fetch do CUBO
+  fetch("../Components/Cubo3D/cubo3D.html")
+  .then( response => {
+    return response.text()
+  })
+  .then(data => {
+    document.querySelector("#campoDo3D").innerHTML = data
+    resolve()
+  })
+})
