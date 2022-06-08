33af680eeba4eb0091b6d846e7d93fba1e60f589
Anderson <anderson.scr@outlook.com>
2022-06-08 19:16:20 -0400

    tentando fix pro CORS

--- Database/server.js
+++ Database/server.js
@@ -8,6 +8,11 @@ const dotenv = require("dotenv")
 dotenv.config()
 
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
